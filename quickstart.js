5addb537ef04bb0580960aa61eb8ffa520fe50d7
Rebecca Mills <beccamills86@gmail.com>
2019-05-30 10:58:38 -0230

    updated quickstart.js

--- quickstart.js
+++ quickstart.js
@@ -1,4 +1,6 @@
 const cassandra = require('cassandra-driver');
+
+// TO DO: Fill in your own host and data center
 const client = new cassandra.Client({ 
   contactPoints: ['127.0.0.1'], 
   localDataCenter: 'datacenter1',  
@@ -12,16 +14,17 @@ function insertUser(lastname, age, city, email, firstname) {
 
 function selectUser(lastname) {
   
-   // TO DO: execute a prepared that retrieves one user from the table
+  // TO DO: execute a prepared that retrieves one user from the table
 }
 
 function updateUser(age, lastname) {
   
-    // TO DO: execute a prepared statement that updates the age of one user
+  // TO DO: execute a prepared statement that updates the age of one user
 }
 
 function deleteUser(lastname) {
-    // TO DO: execute a simple statement that deletes one user from the table
+  
+  // TO DO: execute a simple statement that deletes one user from the table
 }
 
 async function example() {
